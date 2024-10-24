d51dd306b129cd5b21a60477f14164e050251368
MikuuBluee <shinzui079@gmail.com>
2024-10-24 14:00:03 +0700

    selesai

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 function runner() {
 
   // TULIS SCRIPT KAMU DI SINI
+  let namaAtlet = ['Rizki', 'Ambar', 'Rada', 'Tika', 'Malik', 'Nanang', 'Azizah'];
+  let tinggiAtlet = ['150', '155', '148', '165', '170', '168', '166']
 
+  for (i = 0; i < 7; i++) {
 
-
+    console.log(`Atlet ${namaAtlet[i]} tinggi badannya ${tinggiAtlet[i]}cm`);
+  }
 
 
 
   /// JANGAN UBAH SELAINNYA
 }
-
+runner()
 module.exports = runner
